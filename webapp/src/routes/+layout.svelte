bd302ea6b4ba900d09fb9d968d5bbafa4f967889
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-04-23 16:47:20 +0200

    fix: revert change

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -6,10 +6,8 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 <script lang="ts">
 	import '../app.css';
-	import { page } from '$app/state';
 	import { appName } from '@/brand';
 	import { Toaster } from '@/components/ui/sonner';
-	import { locales, localizeHref } from '@/i18n';
 
 	let { children } = $props();
 </script>
@@ -19,14 +17,6 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 		{appName}
 	</title>
 </svelte:head>
-
-<!-- Needed for site crawling for prerendering -->
-<!-- https://inlang.com/m/gerre34r/library-inlang-paraglideJs/server-side-rendering#site-crawling-invisible-anchor-tags -->
-<div style="display: none">
-	{#each locales as locale}
-		<a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
-	{/each}
-</div>
 
 {@render children()}
 
