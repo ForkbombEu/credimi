e870780e288312cb51886c2fd30cded3dff198b9
FilippoTrotter <filippotrotter99@gmail.com>
2025-04-29 15:29:02 +0200

    feat: add ewc temporal workflow

---
+++ pkg/workflowengine/workflows/ewc.go
@@ -0,0 +1,275 @@
+// SPDX-FileCopyrightText: 2025 Forkbomb BV
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// Package workflows provides implementations of workflows for the OpenID certification site.
+// It includes the OpenIDNetWorkflow for conformance checks and the OpenIDNetLogsWorkflow
+// for draining logs from the OpenID certification site.
+package workflows
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/workflow"
+
+	"github.com/forkbombeu/credimi/pkg/internal/temporalclient"
+	"github.com/forkbombeu/credimi/pkg/workflowengine"
+	"github.com/forkbombeu/credimi/pkg/workflowengine/activities"
+)
+
+// EWCTaskQueue is the task queue for EWC workflows.
+const EWCTaskQueue = "EWCTaskQueue"
+
+// EWCWorkflow is a workflow that performs conformance checks on the OpenID certification site.
+type EWCWorkflow struct{}
+
+// Name returns the name of the EWCWorkflow.
+func (EWCWorkflow) Name() string {
+	return "Conformance check on EWC"
+}
+
+// GetOptions Configure sets up the workflow with the necessary options.
+func (EWCWorkflow) GetOptions() workflow.ActivityOptions {
+	return DefaultActivityOptions
+}
+
+type ResponseBody struct {
+	Status    string   `json:"status"`
+	Reason    string   `json:"reason"`
+	SessionID string   `json:"sessionId"`
+	Claims    []string `json:"claims,omitempty"`
+}
+
+// Workflow is the main workflow function for the EWCWorkflow. It orchestrates
+// the execution of various activities to perform conformance checks
+// and send notifications to the user.
+//
+// Parameters:
+//   - ctx: The workflow context used to manage workflow execution.
+//   - input: The input data for the workflow, including payload and configuration.
+//
+// Returns:
+//   - workflowengine.WorkflowResult: The result of the workflow execution, including
+//     a message and log data.
+//   - error: An error if the workflow fails at any step.
+//
+// Workflow Steps:
+//  1. Execute the StepCIWorkflowActivity to perform initial checks and gets the QR code deep link.
+//  2. Generate a URL with query parameters for the user to continue the process.
+//  3. Configure and execute the SendMailActivity to notify the user via email.
+//  4. Wait for a signal ("ewc-check-started") to start polling the API to getthe current status of the check.
+//  5. Wait for either a signal ("ewc-check-stopped") to pause the workflow or the check result from the API
+//  6. Process the response to determine the success or failure of the workflow.
+//
+// Notes:
+//   - The workflow uses a selector to wait for either a signal or the next API call.
+//   - If the signal data indicates failure, the workflow terminates with a failure message.
+func (w *EWCWorkflow) Workflow(
+	ctx workflow.Context,
+	input workflowengine.WorkflowInput,
+) (workflowengine.WorkflowResult, error) {
+	logger := workflow.GetLogger(ctx)
+	ctx = workflow.WithActivityOptions(ctx, w.GetOptions())
+
+	stepCIWorkflowActivity := activities.StepCIWorkflowActivity{}
+	stepCIInput := workflowengine.ActivityInput{
+		Payload: map[string]any{
+			"yaml": input.Payload["yaml"],
+		},
+	}
+	var stepCIResult workflowengine.ActivityResult
+	err := workflow.ExecuteActivity(ctx, stepCIWorkflowActivity.Name(), stepCIInput).
+		Get(ctx, &stepCIResult)
+	if err != nil {
+		logger.Error("StepCIExecution failed", "error", err)
+		return workflowengine.WorkflowResult{}, err
+	}
+	result, ok := stepCIResult.Output.(map[string]any)
+	if !ok {
+		return workflowengine.WorkflowResult{}, fmt.Errorf("unexpected output type: %T", stepCIResult.Output)
+	}
+	deepLink, ok := result["deep_link"].(string)
+	if !ok {
+		return workflowengine.WorkflowResult{}, fmt.Errorf("missing deep_link in stepci response")
+	}
+	sessionID, ok := result["session_id"].(string)
+	if !ok {
+		return workflowengine.WorkflowResult{}, fmt.Errorf("missing session_id in stepci response")
+	}
+	baseURL := input.Payload["app_url"].(string) + "/tests/wallet"
+	u, err := url.Parse(baseURL)
+	if err != nil {
+		return workflowengine.WorkflowResult{}, fmt.Errorf("unexpected error parsing URL: %v", err)
+	}
+	query := u.Query()
+	query.Set("workflow-id", workflow.GetInfo(ctx).WorkflowExecution.ID)
+	query.Set("qr", deepLink)
+	u.RawQuery = query.Encode()
+	emailActivity := activities.SendMailActivity{}
+
+	emailInput := workflowengine.ActivityInput{
+		Config: map[string]string{
+			"recipient": input.Payload["user_mail"].(string),
+		},
+		Payload: map[string]any{
+			"subject": "[CREDIMI] Action required to continue your conformance checks",
+			"body": fmt.Sprintf(`
+		<html>
+			<body>
+				<p>Please click on the following link:</p>
+				<p><a href="%s" target="_blank" rel="noopener">%s</a></p>
+			</body>
+		</html>
+	`, u.String(), u.String()),
+		},
+	}
+	err = emailActivity.Configure(&emailInput)
+	if err != nil {
+		logger.Error("Email activity configure failed", "error", err)
+		return workflowengine.WorkflowResult{}, err
+	}
+	err = workflow.ExecuteActivity(ctx, emailActivity.Name(), emailInput).Get(ctx, nil)
+	if err != nil {
+		logger.Error("Failed to send mail to user ", "error", err)
+		return workflowengine.WorkflowResult{}, err
+	}
+
+	startSignalChan := workflow.GetSignalChannel(ctx, "start-ewc-check-signal")
+	stopSignalChan := workflow.GetSignalChannel(ctx, "stop-ewc-check-signal")
+
+	polling := false
+
+	for {
+		selector := workflow.NewSelector(ctx)
+
+		selector.AddReceive(startSignalChan, func(c workflow.ReceiveChannel, _ bool) {
+			var signalData struct{}
+			c.Receive(ctx, &signalData)
+			polling = true
+		})
+
+		selector.AddReceive(stopSignalChan, func(c workflow.ReceiveChannel, _ bool) {
+			var signalData struct{}
+			c.Receive(ctx, &signalData)
+			polling = false
+		})
+
+		if polling {
+
+			var HTTPGetActivity activities.HTTPActivity
+			var response workflowengine.ActivityResult
+			HTTPInput := workflowengine.ActivityInput{
+				Config: map[string]string{
+					"method": "GET",
+					"url":    "https://ewc.api.forkbomb.eu/issueStatus",
+				},
+				Payload: map[string]any{
+					"query_params": map[string]any{
+						"sessionId": sessionID,
+					},
+				},
+			}
+			err := workflow.ExecuteActivity(ctx, HTTPGetActivity.Name(), HTTPInput).Get(ctx, &response)
+			if err != nil {
+				logger.Error("HTTP GET failed", "error", err)
+				return workflowengine.WorkflowResult{}, err
+			}
+			bodyJSON, err := json.Marshal(response.Output.(map[string]any)["body"])
+			if err != nil {
+				return workflowengine.WorkflowResult{}, fmt.Errorf("failed to re-marshal body: %w", err)
+			}
+			var parsed ResponseBody
+			err = json.Unmarshal(bodyJSON, &parsed)
+			if err != nil {
+				logger.Error("Failed to parse JSON response", "error", err)
+				return workflowengine.WorkflowResult{}, err
+			}
+
+			switch parsed.Status {
+			case "success":
+				return workflowengine.WorkflowResult{
+					Message: "EWC check completed successfully",
+					Log:     parsed.Claims,
+				}, nil
+
+			case "pending":
+				if parsed.Reason != "ok" {
+					return workflowengine.WorkflowResult{
+						Message: fmt.Sprintf("EWC check failed: %s", parsed.Reason),
+					}, nil
+				}
+			case "failed":
+				return workflowengine.WorkflowResult{
+					Message: fmt.Sprintf("EWC check failed: %s", parsed.Reason),
+				}, nil
+
+			default:
+				return workflowengine.WorkflowResult{}, fmt.Errorf("unexpected status from 'https://ewc.api.forkbomb.eu/verificationStatus': %s", parsed.Status)
+			}
+
+		}
+
+		// Wait for either a signal or a timer
+		selector.AddFuture(workflow.NewTimer(ctx, time.Second), func(f workflow.Future) {
+		})
+
+		selector.Select(ctx)
+	}
+}
+
+// Start initializes and starts the EWCWorkflow execution.
+// It loads environment variables, configures the Temporal client with the specified namespace,
+// and sets up workflow options including a unique workflow ID and optional memo.
+// The workflow is then executed with the provided input.
+//
+// Parameters:
+//   - input: A WorkflowInput object containing configuration and input data for the workflow.
+//
+// Returns:
+//   - result: A WorkflowResult object (currently empty in this implementation).
+//   - err: An error if the workflow fails to start or if there is an issue with the Temporal client.
+//
+// Notes:
+//   - The namespace defaults to "default" if not provided in the input configuration.
+//   - The workflow ID is generated using a UUID to ensure uniqueness.
+//   - The Temporal client is closed after the workflow execution is initiated.
+func (w *EWCWorkflow) Start(
+	input workflowengine.WorkflowInput,
+) (result workflowengine.WorkflowResult, err error) {
+	// Load environment variables.
+	godotenv.Load()
+	namespace := "default"
+	if input.Config["namespace"] != nil {
+		namespace = input.Config["namespace"].(string)
+	}
+	c, err := temporalclient.GetTemporalClientWithNamespace(
+		namespace,
+	)
+	if err != nil {
+		return workflowengine.WorkflowResult{}, fmt.Errorf("unable to create client: %v", err)
+	}
+	defer c.Close()
+
+	workflowOptions := client.StartWorkflowOptions{
+		ID:        "OpenIDTestWorkflow" + uuid.NewString(),
+		TaskQueue: OpenIDNetTaskQueue,
+	}
+	if input.Config["memo"] != nil {
+		workflowOptions.Memo = input.Config["memo"].(map[string]any)
+	}
+
+	// Start the workflow execution.
+	_, err = c.ExecuteWorkflow(context.Background(), workflowOptions, w.Name(), input)
+	if err != nil {
+		return workflowengine.WorkflowResult{}, fmt.Errorf("failed to start workflow: %v", err)
+	}
+
+	return workflowengine.WorkflowResult{}, nil
+}
