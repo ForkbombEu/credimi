e1db08488a3782c34348882cdd5adce0a8ee15d7
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-05-14 12:36:27 +0200

    update frontend query

--- webapp/src/lib/utils/index.ts
+++ webapp/src/lib/utils/index.ts
@@ -6,6 +6,8 @@ import { error } from '@sveltejs/kit';
 import { loadFeatureFlags } from '@/features';
 import { verifyUser } from '@/auth/verifyUser';
 import { redirect } from '@/i18n';
+import { pb } from '@/pocketbase';
+import { PocketbaseQueryAgent } from '@/pocketbase/query';
 
 //
 
@@ -27,4 +29,34 @@ export async function checkAuthFlagAndUser(options: {
 	const featureFlags = await loadFeatureFlags(fetchFn);
 	if (!featureFlags.AUTH) onAuthError();
 	if (!(await verifyUser(fetchFn))) onUserError();
+}
+
+//
+
+export async function getUserOrganization(options = { fetch }) {
+	const authorizationQuery = new PocketbaseQueryAgent(
+		{
+			collection: 'orgAuthorizations',
+			filter: `user.id = "${pb.authStore.record?.id}"`,
+			expand: ['organization']
+		},
+		{
+			fetch: options.fetch
+		}
+	);
+	const authorization = (await authorizationQuery.getFullList()).at(0);
+	if (!authorization || !authorization.expand?.organization) return undefined;
+
+	try {
+		const organizationInfo = await pb
+			.collection('organization_info')
+			.getFirstListItem(`owner.id = "${authorization.organization}"`, {
+				fetch: options.fetch
+			});
+
+		return { organizationInfo, organization: authorization.expand.organization };
+	} catch (e) {
+		console.log(e);
+		return undefined;
+	}
 }
--- webapp/src/routes/my/services-and-products/+page.ts
+++ webapp/src/routes/my/services-and-products/+page.ts
@@ -2,24 +2,8 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { pb } from '@/pocketbase';
-import type { OrganizationInfoResponse } from '@/pocketbase/types';
-import { error } from '@sveltejs/kit';
+import { getUserOrganization } from '$lib/utils/index.js';
 
 export const load = async ({ fetch }) => {
-	const userId = pb.authStore.record?.id;
-	if (!userId) error(500);
-
-	let organizationInfo: OrganizationInfoResponse | undefined = undefined;
-	try {
-		organizationInfo = await pb
-			.collection('organization_info')
-			.getFirstListItem(`owner = '${userId}'`, { fetch });
-	} catch (e) {
-		console.log(e);
-	}
-
-	return {
-		organizationInfo
-	};
+	return await getUserOrganization({ fetch });
 };
