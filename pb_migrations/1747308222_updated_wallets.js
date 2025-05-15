90010e002ebf22747887fc60977de7330f23d5c1
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-05-15 17:58:07 +0200

    fix: wallet schema

---
+++ pb_migrations/1747308222_updated_wallets.js
@@ -0,0 +1,46 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_120182150")
+
+  // update collection data
+  unmarshal({
+    "createRule": "@collection.orgAuthorizations.user.id ?= @request.auth.id &&\n@collection.orgAuthorizations.organization.id ?= owner.id &&\n@collection.orgAuthorizations.role.name ?= \"owner\"",
+    "deleteRule": "@collection.orgAuthorizations.user.id ?= @request.auth.id &&\n@collection.orgAuthorizations.organization.id ?= owner.id &&\n@collection.orgAuthorizations.role.name ?= \"owner\"",
+    "listRule": "published = true || (\n  @collection.orgAuthorizations.user.id ?= @request.auth.id &&\n  @collection.orgAuthorizations.organization.id ?= owner.id\n)",
+    "updateRule": "@collection.orgAuthorizations.user.id ?= @request.auth.id &&\n@collection.orgAuthorizations.organization.id ?= owner.id &&\n@collection.orgAuthorizations.role.name ?= \"owner\"",
+    "viewRule": "published = true || (\n  @collection.orgAuthorizations.user.id ?= @request.auth.id &&\n  @collection.orgAuthorizations.organization.id ?= owner.id\n)"
+  }, collection)
+
+  // add field
+  collection.fields.addAt(1, new Field({
+    "cascadeDelete": false,
+    "collectionId": "aako88kt3br4npt",
+    "hidden": false,
+    "id": "relation3479234172",
+    "maxSelect": 1,
+    "minSelect": 0,
+    "name": "owner",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_120182150")
+
+  // update collection data
+  unmarshal({
+    "createRule": "@request.auth.id != \"\"",
+    "deleteRule": "",
+    "listRule": "published = true",
+    "updateRule": "",
+    "viewRule": "published = true"
+  }, collection)
+
+  // remove field
+  collection.fields.removeById("relation3479234172")
+
+  return app.save(collection)
+})
