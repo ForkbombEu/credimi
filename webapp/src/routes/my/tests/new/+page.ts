42d711778193f02df9b043c08349486ae9a1ec4b
phoebus-84 <83974413+phoebus-84@users.noreply.github.com>
2025-05-23 18:49:34 +0200

    fix: custom checks should be displayed in test only if I am the owner (#300)
    * fix: custom checks should be displayed in test only if I am the owner
    
    * fix: layout for test/new broken

--- webapp/src/routes/my/tests/new/+page.ts
+++ webapp/src/routes/my/tests/new/+page.ts
@@ -8,6 +8,7 @@ import { error } from '@sveltejs/kit';
 import { Either } from 'effect';
 import { pb } from '@/pocketbase/index.js';
 import { checkAuthFlagAndUser } from '$lib/utils/index.js';
+import { PocketbaseQueryAgent } from '@/pocketbase/query/agent.js';
 
 //
 
@@ -15,10 +16,23 @@ export const load = async ({ fetch }) => {
 	await checkAuthFlagAndUser({ fetch });
 
 	const result = await getStandardsAndTestSuites({ fetch });
+	const organizationAuth = await new PocketbaseQueryAgent(
+		{
+			collection: 'orgAuthorizations',
+			expand: ['organization'],
+			filter: `user.id = "${pb.authStore.record?.id}"`
+		},
+		{ fetch }
+	).getFullList();
+
+	const organization = organizationAuth.at(0)?.expand?.organization;
+	if (!organization) error(500, { message: 'USER_MISSING_ORGANIZATION' });
 
 	let customChecks: CustomChecksResponse[] = [];
 	try {
-		customChecks = await pb.collection('custom_checks').getFullList();
+		customChecks = await pb
+			.collection('custom_checks')
+			.getFullList({ filter: `owner = '${organization.id}'` });
 	} catch (e) {
 		console.error(e);
 	}
