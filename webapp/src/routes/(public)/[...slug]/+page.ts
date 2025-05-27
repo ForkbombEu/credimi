3f3eb0962d1ffedefc2d4e8d020c8b33d4dae7d7
phoebus-84 <ennio@dyne.org>
2025-05-27 12:31:17 +0200

    fix: handle slug errors better

--- webapp/src/routes/(public)/[...slug]/+page.ts
+++ webapp/src/routes/(public)/[...slug]/+page.ts
@@ -26,16 +26,16 @@ export async function load({ params, fetch }) {
 						if (!response.ok) {
 							
 							if (response.status === 404) {
-								throw new FileFetchError(`Page not found: ${params.slug.join('/')}`);
+								throw new FileFetchError(`Page not found: ${params.slug}`);
 							} else {
-								throw new FileFetchError(
-									`Server error fetching page ${params.slug.join('/')}: ${response.statusText}`
+								throw new FileParseError(
+									`Server error fetching page ${params.slug}: ${response.statusText}`
 								);
 							}
 						}
 						if (contentType && contentType.toLowerCase().includes('text/html')) {
 							throw new FileFetchError(
-								`Expected markdown file for page ${params.slug.join('/')}, but received HTML.`
+								`Expected markdown file for page ${params.slug}, but received HTML.`
 							);
 						}
 						return response.text()},
@@ -67,3 +67,4 @@ export async function load({ params, fetch }) {
 }
 
 class FileFetchError extends Error {}
+class FileParseError extends Error {}
