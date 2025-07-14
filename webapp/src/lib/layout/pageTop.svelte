0be0c1216aa6074e2435eb3358e8adef0aef1e0d
seabassdk <s.blichfeldt@gmail.com>
2025-07-14 08:10:59 +0200

    fixed pageTop to be responsive

--- webapp/src/lib/layout/pageTop.svelte
+++ webapp/src/lib/layout/pageTop.svelte
@@ -17,7 +17,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 </script>
 
 <div class={['border-y-primary border-y-2 bg-[url(/hero.png)] bg-cover bg-right', containerClass]}>
-	<div class={['mx-auto max-w-screen-xl space-y-8 px-8 py-12 pt-20', contentClass]}>
+	<div class={['mx-auto max-w-screen-xl px-4 py-6 space-y-6 pt-8 md:px-8 md:py-12 sm:space-y-8 md:pt-20', contentClass]}>
 		{@render children()}
 	</div>
 </div>
