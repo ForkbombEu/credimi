15546224fe5975878b03df17ade3e579c0e8b4ff
seabassdk <s.blichfeldt@gmail.com>
2025-07-14 16:18:17 +0200

    fixed responsiveness in footer in my tests new

--- webapp/src/lib/start-checks-form/_utils/footer.svelte
+++ webapp/src/lib/start-checks-form/_utils/footer.svelte
@@ -19,12 +19,12 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 <div
 	class={['bg-background sticky bottom-0 rounded-md border p-3 px-6 backdrop-blur-lg', className]}
 >
-	<div class="mx-auto flex max-w-screen-xl items-center justify-between gap-8">
+	<div class="mx-auto flex max-w-screen-xl flex-col gap-4 md:flex-row md:items-center md:justify-between md:gap-8">
 		<div class="flex shrink-0 text-sm">
 			{@render left?.()}
 		</div>
 
-		<div class="flex items-start gap-4">
+		<div class="flex flex-wrap items-start gap-2 md:gap-4">
 			{@render right?.()}
 		</div>
 	</div>
