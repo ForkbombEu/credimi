d7dfbe14a31ac3b6a7320a28713d05e1c51a3262
seabassdk <s.blichfeldt@gmail.com>
2025-07-14 08:20:20 +0200

    fixed home page responsive

--- webapp/src/routes/(public)/+page.svelte
+++ webapp/src/routes/(public)/+page.svelte
@@ -71,7 +71,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 			</T>
 		</div>
 	</div>
-	<div class="flex gap-4">
+	<div class="flex flex-col md:flex-row gap-4">
 		<Button variant="default" href="/marketplace">
 			{m.Explore_Marketplace()}
 		</Button>
@@ -99,14 +99,14 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 				filter: `type != '${excludeFromSolutions}'`
 			}}
 			hide={['pagination']}
-		>
-			{#snippet records({ records })}
-				<PageGrid>
-					{#each records as item, i}
-						<MarketplaceItemCard {item} class={'last:hidden last:lg:flex'} />
-					{/each}
-				</PageGrid>
-			{/snippet}
+		>		{#snippet records({ records })}
+			<PageGrid>
+				{#each records as item, i}
+					{@const isLast = i === MAX_SOLUTION_ITEMS - 1}
+					<MarketplaceItemCard {item} class={isLast ? 'hidden lg:flex' : ''} />
+				{/each}
+			</PageGrid>
+		{/snippet}
 		</CollectionManager>
 	</div>
 	<MarketplaceSection
