6547e393ca183bc5e6cdb3d478c05018c03c9161
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-07-29 12:15:50 +0200

    fix: temp disable edit button

--- webapp/src/routes/(public)/marketplace/(common)/+layout.svelte
+++ webapp/src/routes/(public)/marketplace/(common)/+layout.svelte
@@ -15,11 +15,6 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 	import { userOrganization } from '$lib/app-state';
 	import Button from '@/components/ui-custom/button.svelte';
 	import { PencilIcon } from 'lucide-svelte';
-	import Sheet from '@/components/ui-custom/sheet.svelte';
-	import { CollectionForm } from '@/collections-components';
-	import { pb } from '@/pocketbase';
-	import Spinner from '@/components/ui-custom/spinner.svelte';
-
 	//
 
 	let { children, data } = $props();
@@ -30,19 +25,21 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 	const isCurrentUserOwner = $derived(
 		userOrganization.current?.id === marketplaceItem.organization_id
 	);
-
-	let isEditing = $state(false);
 </script>
 
 {#if isCurrentUserOwner}
 	<div class="border-t-primary border-t-2 bg-[#E2DCF8] py-2">
-		<div class="mx-auto flex max-w-screen-xl flex-wrap items-center gap-3 px-4 text-sm md:px-8">
+		<div
+			class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between gap-3 px-4 text-sm md:px-8"
+		>
 			<T>{m.This_item_is_yours({ item: display.label })}</T>
-			<Button size="sm" class="!h-8 text-xs" onclick={() => (isEditing = true)}>
-				<PencilIcon />
-				{m.Make_changes()}
-			</Button>
-			<T>{m.Last_edited()}: {new Date(marketplaceItem.updated).toLocaleDateString()}</T>
+			<div class="flex items-center gap-3">
+				<T>{m.Last_edited()}: {new Date(marketplaceItem.updated).toLocaleDateString()}</T>
+				<Button size="sm" class="!h-8 text-xs" disabled>
+					<PencilIcon />
+					{m.Make_changes()}
+				</Button>
+			</div>
 		</div>
 	</div>
 {/if}
@@ -71,21 +68,3 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 <PageContent class="bg-secondary grow" contentClass="flex flex-col md:flex-row gap-12 items-start">
 	{@render children()}
 </PageContent>
-
-<Sheet bind:open={isEditing} hideTrigger>
-	{#snippet content({ closeSheet })}
-		<div class="flex flex-col gap-4">
-			{#await pb.collection(marketplaceItem.type).getOne(marketplaceItem.id)}
-				<Spinner />
-			{:then record}
-				<CollectionForm
-					collection={marketplaceItem.type}
-					recordId={marketplaceItem.id}
-					initialData={record}
-				/>
-			{:catch error}
-				<T>{error.message}</T>
-			{/await}
-		</div>
-	{/snippet}
-</Sheet>
