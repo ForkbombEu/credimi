40f391c0b26d74c1569c30d478e6cb7e82c5ec24
phoebus-84 <ennio@dyne.org>
2025-08-06 11:48:47 +0200

    fix: reomve uninteded changes to checks_handlers

--- pkg/internal/apis/handlers/checks_handlers.go
+++ pkg/internal/apis/handlers/checks_handlers.go
@@ -302,9 +302,26 @@ func HandleGetMyCheckRun() func(*core.RequestEvent) error {
 				err.Error(),
 			)
 		}
-
-		// Directly return the Temporal response
-		return e.JSON(http.StatusOK, workflowExecution)
+		weJSON, err := protojson.Marshal(workflowExecution)
+		if err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"workflow",
+				"failed to marshal workflow execution",
+				err.Error(),
+			)
+		}
+		finalJSON := make(map[string]interface{})
+		err = json.Unmarshal(weJSON, &finalJSON)
+		if err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"workflow",
+				"failed to unmarshal workflow execution",
+				err.Error(),
+			)
+		}
+		return e.JSON(http.StatusOK, finalJSON)
 	}
 }
 
@@ -455,9 +472,39 @@ func HandleListMyCheckRuns() func(*core.RequestEvent) error {
 				err.Error(),
 			)
 		}
-
-		// Directly return the Temporal response
-		return e.JSON(http.StatusOK, list)
+		listJSON, err := protojson.Marshal(list)
+		if err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"workflow",
+				"failed to marshal workflow list",
+				err.Error(),
+			)
+		}
+		finalJSON := make(map[string]interface{})
+		err = json.Unmarshal(listJSON, &finalJSON)
+		if err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"workflow",
+				"failed to unmarshal workflow list",
+				err.Error(),
+			)
+		}
+		if finalJSON["executions"] == nil {
+			finalJSON["executions"] = []any{}
+		}
+		executions, ok := (finalJSON["executions"]).([]any)
+		if !ok {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"workflow",
+				"invalid executions data type",
+				"executions field is not of expected type",
+			)
+		}
+		finalJSON["executions"] = sortExecutionsByStartTime(executions)
+		return e.JSON(http.StatusOK, finalJSON)
 	}
 }
 
