5c3d8255de7e978dc281e881ce5fce4d470f9c55
FilippoTrotter <129587442+FilippoTrotter@users.noreply.github.com>
2025-08-06 15:50:05 +0200

    feat: add vlei checks to start new check (#490)
    Co-authored-by: Andrea D'Intino <andrea@forkbomb.eu>

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -61,7 +61,6 @@ func Setup(app *pocketbase.PocketBase) {
 	apis.HookStartScheduledWorkflow(app)
 	apis.HookCredentialWorkflow(app)
 	apis.HookUpdateCredentialsIssuers(app)
-	apis.HookvLEIWorkflow(app)
 
 	jsvm.MustRegister(app, jsvm.Config{
 		HooksWatch: true,
