26ea0c965022b679322b397e241023823d79da5a
Puria Nafisi Azizi <puria@dyne.org>
2025-08-06 18:00:56 +0200

    fix: Revert "tests runs view (#488)" (#493)
    This reverts commit 28fe6ef8c913a80018d13ce517a77df9733120da.

--- webapp/src/lib/workflows/utils.ts
+++ webapp/src/lib/workflows/utils.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import type { WorkflowExecution } from '$lib/credimiClient';
+import type { WorkflowExecution } from '@forkbombeu/temporal-ui/dist/types/workflows';
 import { Array } from 'effect';
 
 //
