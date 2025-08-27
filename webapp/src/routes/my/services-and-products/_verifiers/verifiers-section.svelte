5c787ab9a52b5dfa212b92f9009aa65824c8ae4f
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-08-27 13:07:53 +0200

    setup stuff

--- webapp/src/routes/my/services-and-products/_verifiers/verifiers-section.svelte
+++ webapp/src/routes/my/services-and-products/_verifiers/verifiers-section.svelte
@@ -7,10 +7,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 <script lang="ts">
 	import { CollectionManager } from '@/collections-components';
 	import { m } from '@/i18n';
-	import type { FieldSnippetOptions } from '@/collections-components/form/collectionFormTypes';
-	import VerifierStandardVersionField from './verifier-standard-version-field.svelte';
-	import { CheckboxField } from '@/forms/fields';
-	import MarkdownField from '@/forms/fields/markdownField.svelte';
+	import { settings } from './verifier-form-settings.svelte';
 	import VerifierCard from './verifier-card.svelte';
 
 	//
@@ -30,26 +27,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 		expand: ['use_cases_verifications_via_verifier'],
 		sort: ['created', 'DESC']
 	}}
-	formFieldsOptions={{
-		exclude: ['owner', 'conformance_checks', 'published'],
-		snippets: {
-			standard_and_version,
-			description
-		},
-		descriptions: {
-			name: m.verifier_field_description_name(),
-			description: m.verifier_field_description_description(),
-			logo: m.verifier_field_description_logo(),
-			url: m.verifier_field_description_url(),
-			repository_url: m.verifier_field_description_repository_url(),
-			standard_and_version: m.verifier_field_description_standard_and_version(),
-			format: m.verifier_field_description_format(),
-			signing_algorithms: m.verifier_field_description_signing_algorithms(),
-			cryptographic_binding_methods:
-				m.verifier_field_description_cryptographic_binding_methods()
-		},
-		order: []
-	}}
+	formFieldsOptions={settings}
 >
 	{#snippet top({ Header })}
 		<Header title="Verifiers" {id}>
@@ -69,11 +47,3 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 		</div>
 	{/snippet}
 </CollectionManager>
-
-{#snippet standard_and_version({ form }: FieldSnippetOptions<'verifiers'>)}
-	<VerifierStandardVersionField {form} />
-{/snippet}
-
-{#snippet description({ form }: FieldSnippetOptions<'verifiers'>)}
-	<MarkdownField {form} name="description" />
-{/snippet}
