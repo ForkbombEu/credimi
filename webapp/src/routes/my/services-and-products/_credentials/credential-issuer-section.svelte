0fb3bbd22ec2f4fa8d854ebb5ff73799931dfa41
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-08-29 18:34:01 +0200

    exclude imported from form fields

--- webapp/src/routes/my/services-and-products/_credentials/credential-issuer-section.svelte
+++ webapp/src/routes/my/services-and-products/_credentials/credential-issuer-section.svelte
@@ -74,7 +74,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 		filter: `owner.id = '${organizationId}'`,
 		sort: ['created', 'DESC']
 	}}
-	editFormFieldsOptions={{ exclude: ['owner', 'url', 'published'] }}
+	editFormFieldsOptions={{ exclude: ['owner', 'url', 'published', 'imported'] }}
 	subscribe="expanded_collections"
 >
 	{#snippet top({ Header })}
