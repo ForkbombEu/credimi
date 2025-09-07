e469206b1df5b274adf1367e9a960c730b68dd4f
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-09-07 16:20:39 +0200

    Update record-form.svelte.test.ts

--- webapp/src/modules/v2/pocketbase/record-form/record-form.svelte.test.ts
+++ webapp/src/modules/v2/pocketbase/record-form/record-form.svelte.test.ts
@@ -2,18 +2,15 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import { describe, expect, test } from 'vitest';
+import { beforeEach, describe, expect, test } from 'vitest';
 
+import { Collections } from '@/pocketbase/types';
 import { pocketbase as pb, pocketbaseCrud } from '@/v2';
 
 describe('Pocketbase Record Form', () => {
 	let form: pb.recordform.Instance<'credential_issuers'>;
 
-	// beforeEach(() => {
-
-	// });
-
-	test('should create a form', () => {
+	beforeEach(() => {
 		form = new pb.recordform.Instance({
 			collection: 'credential_issuers',
 			mode: 'create',
@@ -23,6 +20,37 @@ describe('Pocketbase Record Form', () => {
 				client: pocketbaseCrud.createMockClient()
 			})
 		});
+	});
+
+	test('should create a form', () => {
 		expect(form).toBeDefined();
+	});
+
+	test('should change mode', async () => {
+		await form.changeMode({
+			mode: 'update',
+			record: {
+				id: '1',
+				name: 'Test',
+				created: '2021-01-01',
+				description: 'Test',
+				homepage_url: 'Test',
+				imported: false,
+				logo_url: 'Test',
+				published: false,
+				repo_url: 'Test',
+				workflow_url: 'Test',
+				collectionId: 'credential_issuers',
+				owner: 'Test',
+				updated: '2021-01-01',
+				url: 'Test',
+				collectionName: Collections.CredentialIssuers
+			}
+		});
+		expect(form.currentMode).toEqual('update');
+		console.log(form.form.values);
+		expect(form.form.values).toMatchObject({
+			name: 'Test'
+		});
 	});
 });
