beaa20b39d6574f87f0b5822bdfe188f9004cc2a
seabassdk <s.blichfeldt@gmail.com>
2025-09-17 11:21:25 +0200

    fix: changed name of api to get deeplink

--- webapp/src/routes/my/services-and-products/_verifiers/qr-generation-field.svelte
+++ webapp/src/routes/my/services-and-products/_verifiers/qr-generation-field.svelte
@@ -73,7 +73,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 	}
 
 	async function processYamlAndExtractCredentialOffer(yaml: string) {
-		const res = await pb.send('api/credentials_issuers/get-credential-deeplink', {
+		const res = await pb.send('api/credentials_issuers/get-deeplink', {
 			method: 'POST',
 			body: {
 				yaml
