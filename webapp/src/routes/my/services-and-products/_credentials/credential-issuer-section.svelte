d013d091826eb97287e2bdfd8eb85f402e8f6153
seabassdk <s.blichfeldt@gmail.com>
2025-09-24 18:06:49 +0200

    fix: hide input field 'canonified_name' in credential issuer forms

--- webapp/src/routes/my/services-and-products/_credentials/credential-issuer-section.svelte
+++ webapp/src/routes/my/services-and-products/_credentials/credential-issuer-section.svelte
@@ -73,7 +73,9 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 		filter: `owner.id = '${organizationId}'`,
 		sort: ['created', 'DESC']
 	}}
-	editFormFieldsOptions={{ exclude: ['owner', 'url', 'published', 'imported'] }}
+	editFormFieldsOptions={{
+		exclude: ['owner', 'url', 'published', 'imported', 'canonified_name']
+	}}
 >
 	{#snippet top({ Header, records })}
 		<Header title={m.Credential_issuers()} {id}>
