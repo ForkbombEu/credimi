96b6397dfe6a3399dacb0725a2e04176154272e2
FilippoTrotter <filippotrotter99@gmail.com>
2025-10-08 18:28:28 +0200

    fix: re-add wrongly deleted endpoints

--- go.mod
+++ go.mod
@@ -28,7 +28,6 @@ require (
 	golang.org/x/crypto v0.37.0
 	golang.org/x/sys v0.32.0
 	golang.org/x/text v0.24.0
-	google.golang.org/grpc v1.71.0
 	google.golang.org/protobuf v1.36.6
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gopkg.in/yaml.v3 v3.0.1
@@ -300,6 +299,7 @@ require (
 	golang.org/x/vuln v1.1.4 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250324211829-b45e905df463 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
+	google.golang.org/grpc v1.71.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- pkg/internal/apis/handlers/wallet_handler.go
+++ pkg/internal/apis/handlers/wallet_handler.go
@@ -6,10 +6,15 @@ package handlers
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/forkbombeu/credimi/pkg/internal/apierror"
+	"github.com/forkbombeu/credimi/pkg/internal/canonify"
 	"github.com/forkbombeu/credimi/pkg/internal/middlewares"
 	"github.com/forkbombeu/credimi/pkg/internal/routing"
 	"github.com/forkbombeu/credimi/pkg/internal/temporalclient"
@@ -17,6 +22,7 @@ import (
 	"github.com/forkbombeu/credimi/pkg/workflowengine/workflows"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/filesystem"
 	"github.com/pocketbase/pocketbase/tools/hook"
 )
 
@@ -35,9 +41,42 @@ var WalletRoutes routing.RouteGroup = routing.RouteGroup{
 		},
 	},
 }
+var WalletPublicRoutes routing.RouteGroup = routing.RouteGroup{
+	BaseURL:                "/api/wallet",
+	AuthenticationRequired: false,
+	Middlewares: []*hook.Handler[*core.RequestEvent]{
+		{Func: middlewares.ErrorHandlingMiddleware},
+	},
+	Routes: []routing.RouteDefinition{
+		{
+			Method:        http.MethodPost,
+			Path:          "/get-apk-and-action",
+			Handler:       HandleWalletGetApkAndAction,
+			RequestSchema: WalletApkRequest{},
+		},
+		{
+			Method:        http.MethodPost,
+			Path:          "/store-action-result",
+			Handler:       HandleWalletStoreActionResult,
+			RequestSchema: WalletStoreResult{},
+		},
+	},
+}
 
 type WalletURL struct {
 	URL string `json:"walletURL"`
+}
+
+type WalletApkRequest struct {
+	WalletIdentifier string `json:"wallet_identifier"`
+	ActionIdentifier string `json:"action_identifier"`
+	Org              string `json:"organization"`
+}
+
+type WalletStoreResult struct {
+	ResultPath       string `json:"result_path"`
+	ActionIdentifier string `json:"action_identifier"`
+	Org              string `json:"organization"`
 }
 
 func HandleWalletStartCheck() func(*core.RequestEvent) error {
@@ -151,6 +190,165 @@ func HandleWalletStartCheck() func(*core.RequestEvent) error {
 			"appstore_url":  appstoreURL,
 			"home_url":      homeURL,
 			"owner":         organization,
+		})
+	}
+}
+
+func HandleWalletGetApkAndAction() func(*core.RequestEvent) error {
+	return func(e *core.RequestEvent) error {
+		app := e.App
+		var req WalletApkRequest
+		if err := json.NewDecoder(e.Request.Body).Decode(&req); err != nil {
+			return apis.NewBadRequestError("invalid JSON input", err)
+		}
+
+		walletRecord, err := canonify.Resolve(app, req.WalletIdentifier)
+		if err != nil {
+			return apierror.New(
+				http.StatusNotFound,
+				"wallet",
+				"wallet not found",
+				err.Error(),
+			).JSON(e)
+		}
+		actionRecord, err := canonify.Resolve(app, req.ActionIdentifier)
+		if err != nil {
+			return apierror.New(
+				http.StatusNotFound,
+				"wallet_action",
+				"wallet action not found",
+				err.Error(),
+			).JSON(e)
+		}
+		code := actionRecord.GetString("code")
+
+		walletVersionColl, err := app.FindCollectionByNameOrId("wallet_versions")
+		if err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"wallet_versions",
+				"failed to find wallet_versions collection",
+				err.Error(),
+			).JSON(e)
+		}
+		versionRecord, err := app.FindFirstRecordByFilter(
+			walletVersionColl.Id,
+			"wallet = {:walletID} && owner = {:org}",
+			map[string]any{"walletID": walletRecord.Id, "org": req.Org},
+		)
+		if err != nil {
+			return apierror.New(
+				http.StatusNotFound,
+				"wallet_version",
+				"wallet version not found",
+				err.Error(),
+			).JSON(e)
+		}
+		key := versionRecord.BaseFilesPath() + "/" + versionRecord.GetString("android_installer")
+
+		fsys, err := app.NewFilesystem()
+		if err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"filesystem",
+				"failed to create pocketbase filesystem",
+				err.Error(),
+			).JSON(e)
+		}
+		defer fsys.Close()
+
+		blob, err := fsys.GetFile(key)
+		if err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"filesystem",
+				"failed to get apk file",
+				err.Error(),
+			).JSON(e)
+		}
+		defer blob.Close()
+
+		tmpFile, err := os.CreateTemp("", "*.apk")
+		if err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"filesystem",
+				"failed to create tmp apk file",
+				err.Error(),
+			).JSON(e)
+		}
+		defer tmpFile.Close()
+
+		if _, err := io.Copy(tmpFile, blob); err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"filesystem",
+				"failed to copy apk file",
+				err.Error(),
+			).JSON(e)
+		}
+
+		return e.JSON(http.StatusOK, map[string]any{
+			"code":     code,
+			"apk_path": tmpFile.Name(),
+		})
+	}
+}
+
+func HandleWalletStoreActionResult() func(*core.RequestEvent) error {
+	return func(e *core.RequestEvent) error {
+		app := e.App
+		var req WalletStoreResult
+		if err := json.NewDecoder(e.Request.Body).Decode(&req); err != nil {
+			return apis.NewBadRequestError("invalid JSON input", err)
+		}
+
+		actionRecord, err := canonify.Resolve(app, req.ActionIdentifier)
+		if err != nil {
+			return apierror.New(
+				http.StatusNotFound,
+				"wallet_action",
+				"wallet action not found",
+				err.Error(),
+			).JSON(e)
+		}
+
+		const safeTmpDir = "/tmp/credimi/"
+		absResultPath, err := filepath.Abs(req.ResultPath)
+		if err != nil || !strings.HasPrefix(absResultPath, safeTmpDir) {
+			return apierror.New(
+				http.StatusBadRequest,
+				"filesystem",
+				"invalid result file path",
+				"result file path is not allowed",
+			).JSON(e)
+		}
+
+		f, err := filesystem.NewFileFromPath(absResultPath)
+		if err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"filesystem",
+				"failed to open tmp file",
+				err.Error(),
+			).JSON(e)
+		}
+
+		actionRecord.Set("result", []*filesystem.File{f})
+		if err := app.Save(actionRecord); err != nil {
+			return apierror.New(
+				http.StatusInternalServerError,
+				"wallet_action",
+				"failed to save wallet action with result file",
+				err.Error(),
+			).JSON(e)
+		}
+
+		_ = os.Remove(absResultPath)
+
+		return e.JSON(http.StatusOK, map[string]any{
+			"status": "ok",
+			"id":     actionRecord.Id,
 		})
 	}
 }
