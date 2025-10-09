dfc0fcdf29f6ddf4721e45bb3d9adee0e6d5e72f
FilippoTrotter <filippotrotter99@gmail.com>
2025-10-09 11:59:16 +0200

    fix: activity input

--- pkg/workflowengine/activities/utilities.go
+++ pkg/workflowengine/activities/utilities.go
@@ -1,6 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Forkbomb BV
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
 package activities
 
 import (
+	"context"
 	"fmt"
 	"os"
 
@@ -24,7 +28,7 @@ func (a *CheckFileExistsActivity) Name() string {
 	return a.BaseActivity.Name
 }
 
-func (a *CheckFileExistsActivity) Execute(input workflowengine.ActivityInput) (workflowengine.ActivityResult, error) {
+func (a *CheckFileExistsActivity) Execute(_ context.Context, input workflowengine.ActivityInput) (workflowengine.ActivityResult, error) {
 	path, ok := input.Payload["path"].(string)
 	if !ok {
 		errCode := errorcodes.Codes[errorcodes.MissingOrInvalidPayload]
