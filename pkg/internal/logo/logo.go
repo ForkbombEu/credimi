9a3467448ab98e36e87f159680eb0a76fcd054b0
Giulio Sacchet <giulio@dyne.org>
2025-10-28 14:51:52 +0100

    feat: new logo hooks

---
+++ pkg/internal/logo/logo.go
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2025 Forkbomb BV
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package logo
+
+import (
+	"log"
+
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/filesystem"
+)
+
+func LogoHooks(app core.App) {
+	app.OnRecordCreate().BindFunc(func(e *core.RecordEvent) error {
+		return HandleLogo(e)
+	})
+
+	app.OnRecordUpdate().BindFunc(func(e *core.RecordEvent) error {
+		return HandleLogo(e)
+	})
+}
+
+func HandleLogo(e *core.RecordEvent) error {
+	logoURL := e.Record.GetString("logo_url")
+	if logoURL != "" {
+		file, err := filesystem.NewFileFromURL(e.Context, logoURL)
+		if err != nil {
+			log.Println("Error during the logo download:", err)
+			return e.Next()
+		}
+		e.Record.Set("logo", []*filesystem.File{file})
+	}
+	return e.Next()
+}
