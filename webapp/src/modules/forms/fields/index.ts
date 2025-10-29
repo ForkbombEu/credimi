d192c67a5f4db2cf5d66c1f9cb247bed9e205cd4
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-10-29 12:10:45 +0100

    feat(forms): add logo field

--- webapp/src/modules/forms/fields/index.ts
+++ webapp/src/modules/forms/fields/index.ts
@@ -7,19 +7,21 @@ import CodeEditorField from './codeEditorField.svelte';
 import DateField from './dateField.svelte';
 import Field from './field.svelte';
 import FileField from './fileField.svelte';
+import LogoField from './logo-field.svelte';
 import PlaceholderHighlightCodeEditorField from './placeholderHighlightCodeEditorField.svelte';
 import SelectField from './selectField.svelte';
 import SwitchField from './switchField.svelte';
 import TextareaField from './textareaField.svelte';
 
 export {
+	CheckboxField,
+	CodeEditorField,
+	DateField,
 	Field,
 	FileField,
-	SwitchField,
-	CheckboxField,
-	TextareaField,
+	LogoField,
+	PlaceholderHighlightCodeEditorField,
 	SelectField,
-	DateField,
-	CodeEditorField,
-	PlaceholderHighlightCodeEditorField
+	SwitchField,
+	TextareaField
 };
