dd44999f2365f6e7ca9b3401f18e7d9cb6abaed8
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-11-11 16:45:59 +0100

    fix(pipeline_composer): close ui after metadata save

--- webapp/src/lib/pipeline-form/pipeline-form.svelte.ts
+++ webapp/src/lib/pipeline-form/pipeline-form.svelte.ts
@@ -40,7 +40,12 @@ export class PipelineForm {
 		});
 
 		this.metadataForm = new MetadataForm({
-			initialData: props.pipeline?.metadata
+			initialData: props.pipeline?.metadata,
+			onSubmit: async () => {
+				if (!this.saveAfterMetadataFormSubmit) return;
+				await this.save();
+				this.saveAfterMetadataFormSubmit = false;
+			}
 		});
 	}
 
@@ -58,9 +63,14 @@ export class PipelineForm {
 
 	//
 
+	private saveAfterMetadataFormSubmit = $state(false);
+
 	async save() {
 		if (!this.metadataForm.value) {
 			this.metadataForm.isOpen = true;
+			if (this.props.mode === 'create') {
+				this.saveAfterMetadataFormSubmit = true;
+			}
 		} else {
 			const data: Omit<PipelinesFormData, 'owner'> = {
 				...this.metadataForm.value,
