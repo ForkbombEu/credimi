1e15beee62b5f22410360bf64e2b377cc13e67fa
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-11-24 09:00:23 +0100

    refactor(credentials): rename `logo` to `logo_url`

---
+++ pb_migrations/1763970144_updated_credentials.js
@@ -0,0 +1,42 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_183765882")
+
+  // update field
+  collection.fields.addAt(5, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3570560904",
+    "max": 10000000,
+    "min": 0,
+    "name": "logo_url",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_183765882")
+
+  // update field
+  collection.fields.addAt(5, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3570560904",
+    "max": 10000000,
+    "min": 0,
+    "name": "logo",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})
