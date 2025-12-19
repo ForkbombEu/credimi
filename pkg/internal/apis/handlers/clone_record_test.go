6b15f0b80c3332c5c2bb75da3fc041a752bd2b61
Giulio Sacchet <giulio@dyne.org>
2025-12-19 10:52:45 +0100

    test: new test for clone file function

--- pkg/internal/apis/handlers/clone_record_test.go
+++ pkg/internal/apis/handlers/clone_record_test.go
@@ -6,12 +6,14 @@ package handlers
 
 import (
 	"fmt"
+	"io"
 	"strings"
 	"testing"
 
 	"github.com/forkbombeu/credimi/pkg/internal/canonify"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/tests"
+	"github.com/pocketbase/pocketbase/tools/filesystem"
 	"github.com/stretchr/testify/require"
 )
 
@@ -219,4 +221,138 @@ func TestGetCloneRecord(t *testing.T) {
 	for _, scenario := range scenarios {
 		scenario.Test(t)
 	}
+}
+
+func setupTestApp(t testing.TB) *tests.TestApp {
+	app, err := tests.NewTestApp(testDataDir)
+	require.NoError(t, err)
+	return app
+}
+
+func TestCloneFiles_NoFileFieldValues(t *testing.T) {
+	app := setupTestApp(t)
+	defer app.Cleanup()
+
+	coll, err := app.FindCollectionByNameOrId("credentials")
+	require.NoError(t, err)
+
+	originalRecord := core.NewRecord(coll)
+	newRecord := core.NewRecord(coll)
+
+	err = cloneFiles(app, originalRecord, newRecord, map[string]string{})
+	require.NoError(t, err, "Should succeed with empty file field values")
+}
+
+func TestCloneFiles_SingleFile(t *testing.T) {
+	app := setupTestApp(t)
+	defer app.Cleanup()
+	own, _ := getTestOrgID()
+	coll, err := app.FindCollectionByNameOrId("credentials")
+	require.NoError(t, err)
+
+	collcred, _ := app.FindCollectionByNameOrId("credential_issuers")
+	issuerRecord := core.NewRecord(collcred)
+	issuerRecord.Set("id", "tikklnj1uh32237")
+	issuerRecord.Set("name", "test issuer")
+	issuerRecord.Set("url", "https://test-issuer.example.com")
+	issuerRecord.Set("owner", own)
+	require.NoError(t, app.Save(issuerRecord))
+
+	originalRecord := core.NewRecord(coll)
+	originalRecord.Set("id", "original1234567")
+	originalRecord.Set("owner", own)
+	originalRecord.Set("name", "original record")
+	originalRecord.Set("canonified_name", "original_record")
+	originalRecord.Set("credential_issuer", issuerRecord.Id)
+
+	testData := []byte{
+		0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46, 0x49, 0x46, 0x00, 0x01, 0x01,
+		0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0xFF, 0xDB, 0x00, 0x43, 0x00, 0xFF,
+		0xC9, 0x00, 0x0B, 0x08, 0x00, 0x01, 0x00, 0x01, 0x01, 0x01, 0x11, 0x00, 0xFF,
+		0xCC, 0x00, 0x06, 0x00, 0x10, 0x10, 0x05, 0xFF, 0xDA, 0x00, 0x08, 0x01, 0x01,
+		0x00, 0x00, 0x3F, 0x00, 0xD2, 0xCF, 0x20, 0xFF, 0xD9,
+	}
+
+	testFile, err := filesystem.NewFileFromBytes(testData, "testimage.jpg")
+	require.NoError(t, err)
+
+	originalRecord.Set("logo", testFile)
+	err = app.Save(originalRecord)
+	require.NoError(t, err)
+
+	originalFilename := originalRecord.GetString("logo")
+	require.NotEmpty(t, originalFilename, "Original record should have the file")
+
+	newRecord := core.NewRecord(coll)
+	newRecord.Set("id", "new456789012345")
+	newRecord.Set("owner", own)
+	newRecord.Set("name", "new record copy2345")
+	newRecord.Set("canonified_name", "new_record_copy2345")
+	newRecord.Set("credential_issuer", issuerRecord.Id)
+
+	fileFieldValues := map[string]string{
+		"logo": originalFilename,
+	}
+
+	err = cloneFiles(app, originalRecord, newRecord, fileFieldValues)
+	require.NoError(t, err, "Should clone single file successfully")
+
+	clonedFilename := newRecord.GetString("logo")
+	require.NotEmpty(t, clonedFilename, "New record should have the cloned file")
+
+	fsys, err := app.NewFilesystem()
+	require.NoError(t, err)
+	defer fsys.Close()
+
+	clonedFileKey := newRecord.BaseFilesPath() + "/" + clonedFilename
+	r, err := fsys.GetFile(clonedFileKey)
+	require.NoError(t, err)
+	defer r.Close()
+
+	clonedData, err := io.ReadAll(r)
+	require.NoError(t, err)
+	require.Equal(t, testData, clonedData, "Cloned file should have same content")
+
+}
+
+func TestCloneFiles_FileNotFound(t *testing.T) {
+	app := setupTestApp(t)
+	defer app.Cleanup()
+	own, _ := getTestOrgID()
+	coll, err := app.FindCollectionByNameOrId("credentials")
+	require.NoError(t, err)
+
+	collcred, _ := app.FindCollectionByNameOrId("credential_issuers")
+	issuerRecord := core.NewRecord(collcred)
+	issuerRecord.Set("id", "tikklnj1uh32237")
+	issuerRecord.Set("name", "test issuer notfound")
+	issuerRecord.Set("url", "https://test-issuer-notfound.example.com")
+	issuerRecord.Set("owner", own)
+	require.NoError(t, app.Save(issuerRecord))
+
+	originalRecord := core.NewRecord(coll)
+	originalRecord.Set("id", "original1234567")
+	originalRecord.Set("owner", own)
+	originalRecord.Set("name", "original notfound test")
+	originalRecord.Set("canonified_name", "original_notfound_test")
+	originalRecord.Set("credential_issuer", issuerRecord.Id)
+	require.NoError(t, app.Save(originalRecord))
+
+	newRecord := core.NewRecord(coll)
+	newRecord.Set("id", "new456789012345")
+	newRecord.Set("owner", own)
+	newRecord.Set("name", "new notfound test")
+	newRecord.Set("canonified_name", "new_notfound_test")
+	newRecord.Set("credential_issuer", issuerRecord.Id)
+
+	fileFieldValues := map[string]string{
+		"logo": "non_existent_file_12345.jpg",
+	}
+
+	err = cloneFiles(app, originalRecord, newRecord, fileFieldValues)
+	require.NoError(t, err, "Should not error when file not found")
+
+	logoValue := newRecord.Get("logo")
+	require.Empty(t, logoValue, "Field should be empty when file not found")
+
 }
