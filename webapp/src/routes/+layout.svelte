76f7bc0792d42bd6214daf65d911f0859dbd906a
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-01-21 09:29:10 +0100

    chore: fix prettier config

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -31,7 +31,6 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 <GlobalLoading />
 
-
 <div style="display:none">
 	{#each locales as locale (locale)}
 		<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
