8ea44e1b9a74a292171c47e77ce4ad0c9ca8f46c
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-01-29 10:54:54 +0100

    setup back button

--- webapp/src/lib/layout/back-button.svelte
+++ webapp/src/lib/layout/back-button.svelte
@@ -4,6 +4,27 @@ SPDX-FileCopyrightText: 2025 Forkbomb BV
 SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
+<script module lang="ts">
+	import { afterNavigate } from '$app/navigation';
+
+	const history = $state<URL[]>([]);
+
+	const previousHref = $derived.by(() => {
+		const last = history.at(-1);
+		if (!last) return undefined;
+		return last.pathname + last.search;
+	});
+
+	export function storePreviousPageOnNavigate() {
+		afterNavigate(({ from, to, type }) => {
+			if (type === 'popstate') history.pop();
+			else if (!from?.url.pathname) return;
+			else if (to && to.url.pathname === history.at(-1)?.pathname) history.pop();
+			else history.push(from.url);
+		});
+	}
+</script>
+
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 
@@ -21,9 +42,13 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 	};
 
 	let { href, children, class: className = '' }: Props = $props();
+
+	storePreviousPageOnNavigate();
+
+	const actualHref = $derived(previousHref ?? href);
 </script>
 
-<Button {href} variant="link" class={['gap-1 p-0', className]}>
+<Button href={actualHref} variant="link" class={['gap-1 p-0', className]}>
 	<ArrowLeft size={16} />
 	{#if children}
 		{@render children()}
