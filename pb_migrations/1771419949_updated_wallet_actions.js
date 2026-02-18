28290339c1b950cc8f819a30ad96db83b5c5ffa2
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-02-18 14:06:14 +0100

    feat: add "category" to credential

---
+++ pb_migrations/1771419949_updated_wallet_actions.js
@@ -0,0 +1,31 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1403167086")
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "hidden": false,
+    "id": "select105650625",
+    "maxSelect": 1,
+    "name": "category",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "select",
+    "values": [
+      "onboarding",
+      "get-credential",
+      "verify-credential",
+      "other"
+    ]
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1403167086")
+
+  // remove field
+  collection.fields.removeById("select105650625")
+
+  return app.save(collection)
+})
