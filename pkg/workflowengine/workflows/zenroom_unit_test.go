65d5e99a0207a25f0666912934424d00e888b4d6
FilippoTrotter <filippotrotter99@gmail.com>
2026-02-18 15:03:34 +0100

    test(workflows): extend zenroom error coverage

--- pkg/workflowengine/workflows/zenroom_unit_test.go
+++ pkg/workflowengine/workflows/zenroom_unit_test.go
@@ -170,3 +170,132 @@ func TestZenroomWorkflowStart(t *testing.T) {
 	require.True(t, strings.HasPrefix(options.ID, "Zenroom-Workflow-"))
 	require.Equal(t, map[string]any{"test": "memo"}, options.Memo)
 }
+
+func TestZenroomWorkflowStartClientError(t *testing.T) {
+	origClient := zenroomTemporalClient
+	t.Cleanup(func() {
+		zenroomTemporalClient = origClient
+	})
+
+	zenroomTemporalClient = func(namespace string) (client.Client, error) {
+		require.Equal(t, DefaultNamespace, namespace)
+		return nil, errors.New("no client")
+	}
+
+	w := NewZenroomWorkflow()
+	_, err := w.Start(workflowengine.WorkflowInput{})
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "unable to create client")
+}
+
+func TestZenroomWorkflowOutputErrors(t *testing.T) {
+	tests := []struct {
+		name       string
+		output     any
+		errCode    string
+		errMessage string
+	}{
+		{
+			name:    "invalid output type",
+			output:  "nope",
+			errCode: errorcodes.Codes[errorcodes.UnexpectedDockerOutput].Code,
+		},
+		{
+			name: "invalid exit code type",
+			output: map[string]any{
+				"containerID": "cont-1",
+				"exitCode":    "bad",
+			},
+			errCode: errorcodes.Codes[errorcodes.UnexpectedDockerOutput].Code,
+		},
+		{
+			name: "invalid stderr type",
+			output: map[string]any{
+				"containerID": "cont-1",
+				"exitCode":    float64(0),
+				"stderr":      123,
+			},
+			errCode: errorcodes.Codes[errorcodes.UnexpectedDockerOutput].Code,
+		},
+		{
+			name: "invalid stdout type",
+			output: map[string]any{
+				"containerID": "cont-1",
+				"exitCode":    float64(0),
+				"stderr":      "",
+				"stdout":      123,
+				"stout":       "bad-output",
+			},
+			errCode: errorcodes.Codes[errorcodes.UnexpectedDockerOutput].Code,
+		},
+		{
+			name: "non-zero exit code",
+			output: map[string]any{
+				"containerID": "cont-1",
+				"exitCode":    float64(2),
+				"stderr":      "boom",
+				"stdout":      "out",
+			},
+			errCode: errorcodes.Codes[errorcodes.ZenroomExecutionFailed].Code,
+		},
+		{
+			name: "invalid stdout JSON",
+			output: map[string]any{
+				"containerID": "cont-1",
+				"exitCode":    float64(0),
+				"stderr":      "",
+				"stdout":      "not-json",
+			},
+			errCode: errorcodes.Codes[errorcodes.JSONUnmarshalFailed].Code,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			testSuite := &testsuite.WorkflowTestSuite{}
+			env := testSuite.NewTestWorkflowEnvironment()
+
+			w := NewZenroomWorkflow()
+			env.RegisterWorkflowWithOptions(w.Workflow, workflow.RegisterOptions{
+				Name: w.Name(),
+			})
+
+			dockerActivity := activities.NewDockerActivity()
+			env.RegisterActivityWithOptions(dockerActivity.Execute, activity.RegisterOptions{
+				Name: dockerActivity.Name(),
+			})
+
+			fake := &fakeDockerClient{}
+			origNewDockerClient := newDockerClient
+			t.Cleanup(func() {
+				newDockerClient = origNewDockerClient
+			})
+			newDockerClient = func() (dockerClient, error) {
+				return fake, nil
+			}
+
+			env.OnActivity(dockerActivity.Name(), mock.Anything, mock.Anything).
+				Return(workflowengine.ActivityResult{
+					Output: tc.output,
+				}, nil)
+
+			input := workflowengine.WorkflowInput{
+				Payload: ZenroomWorkflowPayload{
+					Contract: "contract",
+				},
+				Config: map[string]any{
+					"app_url": "http://app.test",
+				},
+			}
+			env.ExecuteWorkflow(w.Name(), input)
+
+			err := env.GetWorkflowError()
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tc.errCode)
+			if tc.errMessage != "" {
+				require.Contains(t, err.Error(), tc.errMessage)
+			}
+		})
+	}
+}
