0f91aefc89d007c2a15d3c64cbe65cb1ed5e7457
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-04-29 18:40:02 +0200

    feat: add placeholders ui

--- webapp/src/lib/pipeline-form/steps/utils-steps/http-request-step-form.svelte
+++ webapp/src/lib/pipeline-form/steps/utils-steps/http-request-step-form.svelte
@@ -7,16 +7,17 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 <script lang="ts">
 	import type { SelfProp } from '$lib/renderable';
 
+	import CodeEditor from '@/components/ui-custom/codeEditor.svelte';
 	import Select from '@/components/ui-custom/select.svelte';
 	import T from '@/components/ui-custom/t.svelte';
 	import { Button } from '@/components/ui/button';
 	import { Input } from '@/components/ui/input';
-	import { Textarea } from '@/components/ui/textarea';
 	import { m } from '@/i18n';
 
 	import type { HttpRequestStepForm } from './http-request-step-form.svelte.js';
 
 	import WithLabel from '../_partials/with-label.svelte';
+	import PlaceholderButtons from './placeholders/placeholder-buttons.svelte';
 
 	//
 
@@ -42,9 +43,13 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 		/>
 	</WithLabel>
 
-	<WithLabel label={m.Body()} optional>
-		<Textarea id="body" rows={5} placeholder={`{"key": "value"}`} bind:value={form.data.body} />
-	</WithLabel>
+	{#if form.data.method === 'POST' || form.data.method === 'PUT' || form.data.method === 'PATCH'}
+		<WithLabel label={m.Body()} optional>
+			<CodeEditor lang="json" bind:value={form.data.body} />
+		</WithLabel>
+	{/if}
+
+	<PlaceholderButtons />
 
 	<Button class="w-full" disabled={!form.isValid} onclick={() => form.submit()}>
 		<T>{m.Add_step()}</T>
---
+++ webapp/src/lib/pipeline-form/steps/utils-steps/placeholders/placeholder-buttons.svelte
@@ -0,0 +1,28 @@
+<!--
+SPDX-FileCopyrightText: 2026 Forkbomb BV
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script lang="ts">
+	import { m } from '@/i18n';
+
+	import { formatPlaceholder, getPlaceholderDescription, Placeholder } from './utils';
+</script>
+
+<div class="space-y-2 text-xs text-muted-foreground">
+	<p>{m.pipeline_placeholder_description()}</p>
+
+	<ul class="flex flex-col gap-2">
+		{#each Object.values(Placeholder) as placeholder (placeholder)}
+			<li class="flex flex-col gap-0.5">
+				<p
+					class="w-fit self-start rounded-md bg-slate-100 px-1.5 py-0.5 font-mono text-[11px]"
+				>
+					{formatPlaceholder(placeholder)}
+				</p>
+				<p>{getPlaceholderDescription(placeholder)}</p>
+			</li>
+		{/each}
+	</ul>
+</div>
