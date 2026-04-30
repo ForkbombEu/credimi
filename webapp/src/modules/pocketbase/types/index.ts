c5fc5e3e1ee9c9b170c777a184666c174f9d9eef
Puria Nafisi Azizi <puria@dyne.org>
2026-04-30 07:46:32 +0200

    fix(webapp): fix unit setup and scope CI trigger

--- webapp/src/modules/pocketbase/types/index.ts
+++ webapp/src/modules/pocketbase/types/index.ts
@@ -2,8 +2,8 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-export * from './index.generated';
-export * from './extra.generated';
+export type * from './index.generated';
+export type * from './extra.generated';
 
 //
 
