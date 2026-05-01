5e1325cc15c270207b602033f67340330f8d0d2e
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-05-01 16:04:26 +0200

    feat: add tooltip for runners

--- webapp/src/lib/scoreboard-v2/columns/runners.svelte
+++ webapp/src/lib/scoreboard-v2/columns/runners.svelte
@@ -19,9 +19,23 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 </script>
 
 <script lang="ts">
+	import Tooltip from '@/components/ui-custom/tooltip.svelte';
+
+	import Na from './partials/na.svelte';
+
 	let { value }: Column.Props<typeof column> = $props();
 </script>
 
-{#each value as item (item)}
-	<p class="max-w-[15ch] truncate text-xs">{item.name}</p>
-{/each}
+<div class="flex flex-col items-start">
+	{#each value as item (item)}
+		<Tooltip>
+			<p class="max-w-[15ch] truncate text-xs">{item.name.trim()}</p>
+
+			{#snippet content()}
+				<p>{item.description}</p>
+			{/snippet}
+		</Tooltip>
+	{:else}
+		<Na />
+	{/each}
+</div>
