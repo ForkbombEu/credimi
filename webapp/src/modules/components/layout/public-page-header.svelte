90df58664c5b6c01ae3111cef86ea046a84668ed
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-05-04 20:50:58 +0200

    fix: uniform ui headers

---
+++ webapp/src/modules/components/layout/public-page-header.svelte
@@ -0,0 +1,33 @@
+<!--
+SPDX-FileCopyrightText: 2025 Forkbomb BV
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script lang="ts">
+	import { entities, type EntitySlug } from '$lib/global/entities.js';
+
+	import Icon from '@/components/ui-custom/icon.svelte';
+	import T from '@/components/ui-custom/t.svelte';
+
+	type Props = {
+		entity: EntitySlug;
+		description?: string;
+	};
+
+	let { entity, description }: Props = $props();
+
+	const entityData = $derived(entities[entity]);
+</script>
+
+<div
+	class="mx-auto mb-8 flex max-w-7xl flex-col items-center justify-center gap-4 px-4 pt-10 text-primary md:px-8"
+>
+	<div class="flex items-center gap-2">
+		<Icon src={entityData.icon} class="size-10" />
+		<T tag="h1">
+			{entityData.labels.singular}
+		</T>
+	</div>
+	<p class="max-w-[80ch] text-center text-balance">{description}</p>
+</div>
--- webapp/src/routes/(public)/scoreboard/+page.svelte
+++ webapp/src/routes/(public)/scoreboard/+page.svelte
@@ -1,13 +1,13 @@
 <!--
 SPDX-FileCopyrightText: 2025 Forkbomb BV
+
 SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
 <script lang="ts">
-	import { ChartNoAxesCombined } from '@lucide/svelte';
 	import { Scoreboard } from '$lib';
 
-	import T from '@/components/ui-custom/t.svelte';
+	import PublicPageHeader from '@/components/layout/public-page-header.svelte';
 	import { m } from '@/i18n';
 
 	//
@@ -21,17 +21,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 </script>
 
 <div class="grow bg-secondary pt-0 pb-20">
-	<div
-		class="mx-auto mb-8 flex max-w-7xl flex-col items-center justify-center gap-4 px-4 pt-10 text-primary md:px-8"
-	>
-		<div class="flex items-center gap-2">
-			<ChartNoAxesCombined class="size-10" />
-			<T tag="h1">
-				{m.Scoreboard()}
-			</T>
-		</div>
-		<p class="max-w-[80ch] text-center text-balance">{m.scoreboard_page_description()}</p>
-	</div>
+	<PublicPageHeader entity="scoreboard" description={m.scoreboard_page_description()} />
 	<div class="px-4">
 		<Scoreboard.Component {scoreboard} />
 	</div>
