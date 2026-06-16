f93439f5271d66c5022ecc4bdc349b2e6d98d4f2
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-06-16 16:22:49 +0200

    feat: add manual pipeline editing (#1250)

--- webapp/src/routes/my/pipelines/(group)/[...path]/edit/+page.ts
+++ webapp/src/routes/my/pipelines/(group)/[...path]/edit/+page.ts
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { getEnrichedPipeline } from '$lib/pipeline-form/functions';
-import { getPath } from '$lib/utils/index.js';
+import { getManualEditHref } from '$lib/pipeline/utils';
 
 import { redirect } from '@/i18n/index.js';
 
@@ -11,6 +11,11 @@ import { redirect } from '@/i18n/index.js';
 
 export const load = async ({ fetch, parent }) => {
 	const { pipeline } = await parent();
+
+	if (pipeline.manual) {
+		redirect(getManualEditHref(pipeline));
+	}
+
 	try {
 		const enriched = await getEnrichedPipeline(pipeline.id, { fetch });
 		return {
@@ -18,6 +23,6 @@ export const load = async ({ fetch, parent }) => {
 		};
 	} catch {
 		// If the pipeline is not found, redirect to the manual edit page
-		redirect(`/my/pipelines/${getPath(pipeline, true)}/edit/manual`);
+		redirect(getManualEditHref(pipeline));
 	}
 };
