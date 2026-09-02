dbe9f1a43cdd76a3519ce418ebe8561b4d0ddcc1
matteo-cristino <cristino.matteo97@gmail.com>
2026-09-02 16:38:41 +0200

    test(fcaf): reject non-array transaction data ids
    reason:
    Require invalid transaction data for non-array credential IDs.
    
    prompt:
    Implement WS_RP_MS_ProtocolMessages__026

--- pkg/fcaf/validators/registry.go
+++ pkg/fcaf/validators/registry.go
@@ -63,6 +63,7 @@ func DefaultRegistry() (*Registry, error) {
 		OID4VPVerifierInfoAllCredentialsValidator{},
 		OID4VPTransactionDataCredentialIDsValidator{},
 		OID4VPTransactionDataCredentialIDsMismatchValidator{},
+		OID4VPTransactionDataCredentialIDsNonArrayValidator{},
 		OID4VPWalletNonceMatchValidator{},
 		OID4VPUnsupportedResponseTypeValidator{},
 		OID4VPVerifierMetadataExclusiveValidator{},
